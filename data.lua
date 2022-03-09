ce1e25fd680c70429ae210c415bd7849bfbd379c
wcguo <35400769+wcguo@users.noreply.github.com>
2022-03-10 00:58:23 +0800

    Update data.lua

--- data.lua
+++ data.lua
@@ -8,21 +8,21 @@ require("prototypes.item")
 require("prototypes.item.huanhundan")
 require("prototypes.recipe")
 require("prototypes.technology")
-require("prototypes.lingshi.lingshi-ore")
-require("prototypes.lingshi.lingshi-ore-particle")
-require("prototypes.lingshi.lingshi-gold-ore")
-require("prototypes.lingshi.lingshi-gold-ore-particle")
-require("prototypes.lingshi.lingshi-wood-ore")
-require("prototypes.lingshi.lingshi-wood-ore-particle")
-
-require("prototypes.lingshi.lingshi-water-ore")
-require("prototypes.lingshi.lingshi-water-ore-particle")
-
-require("prototypes.lingshi.lingshi-fire-ore")
-require("prototypes.lingshi.lingshi-fire-ore-particle")
-
-require("prototypes.lingshi.lingshi-earth-ore")
-require("prototypes.lingshi.lingshi-earth-ore-particle")
+#require("prototypes.lingshi.lingshi-ore")
+#require("prototypes.lingshi.lingshi-ore-particle")
+#require("prototypes.lingshi.lingshi-gold-ore")
+#require("prototypes.lingshi.lingshi-gold-ore-particle")
+#require("prototypes.lingshi.lingshi-wood-ore")
+#require("prototypes.lingshi.lingshi-wood-ore-particle")
+#
+#require("prototypes.lingshi.lingshi-water-ore")
+#require("prototypes.lingshi.lingshi-water-ore-particle")
+#
+#require("prototypes.lingshi.lingshi-fire-ore")
+#require("prototypes.lingshi.lingshi-fire-ore-particle")
+#
+#require("prototypes.lingshi.lingshi-earth-ore")
+#require("prototypes.lingshi.lingshi-earth-ore-particle")
 
 
 require("prototypes.auto.AutoGroups")
